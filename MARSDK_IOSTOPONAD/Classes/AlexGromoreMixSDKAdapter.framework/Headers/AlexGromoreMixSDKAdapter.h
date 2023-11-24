9ebd6ae505ee85d2620f609785d210d16e28be67
lizhigang <lizhigang6@163.com>
2023-11-24 15:45:10 +0800

    修复bug

---
+++ MARSDK_IOSTOPONAD/Classes/AlexGromoreMixSDKAdapter.framework/Headers/AlexGromoreMixSDKAdapter.h
@@ -0,0 +1,21 @@
+//
+//  AlexGromoreMixSDKAdapter.h
+//  AlexGromoreMixSDKAdapter
+//
+//  Created by Alex on 8/24/23.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <AlexGromoreMixSDKAdapter/AlexGromoreBaseManager.h>
+#import <AlexGromoreMixSDKAdapter/AlexGromoreExtraConfig.h>
+
+//! Project version number for AlexGromoreMixSDKAdapter.
+FOUNDATION_EXPORT double AlexGromoreMixSDKAdapterVersionNumber;
+
+//! Project version string for AlexGromoreMixSDKAdapter.
+FOUNDATION_EXPORT const unsigned char AlexGromoreMixSDKAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlexGromoreMixSDKAdapter/PublicHeader.h>
+
+
